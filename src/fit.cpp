c8cad69483f94efc28519d0bd023eda5d7c63aa1
Julien Lamy <lamy@unistra.fr>
2021-07-27 13:03:15 +0200

    Switch to a 1D EPG fit

--- src/fit.cpp
+++ src/fit.cpp
@@ -44,7 +44,7 @@ void fit(
             DW_SSFP_subset.data()+block_size*(i+1));
         Problem problem{
             scheme, non_dw, signals, T1_subset[i], T2_subset[i], B1_subset[i],
-            freed};
+            epg_discrete_1d};
         fit(
             problem, algorithm, population, generations, return_tensor,
             local_individuals.data()+item_size*population*i,
