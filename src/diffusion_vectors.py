33b20aacc5a05ba7d4e2a436243862224ea29934
Julien Lamy <lamy@unistra.fr>
2021-07-30 16:33:30 +0200

    Fix compatibility with Python 3.6

--- src/diffusion_vectors.py
+++ src/diffusion_vectors.py
@@ -11,21 +11,29 @@ def read(fd):
         
         line = line.strip()
         
-        if match := re.match(r"^{directions\[(\d+)\]\s*=\s*(\d+)}$", line):
+        match = re.match(r"^{directions\[(\d+)\]\s*=\s*(\d+)}$", line)
+        if match:
             current_set = int(match.group(1))
             sets[current_set] = int(match.group(2))*[None]
-        elif match := re.match(r"^coordinatesystem\[(\d+)\]\s*=\s*(.+)$", line):
+            continue
+        match = re.match(r"^coordinatesystem\[(\d+)\]\s*=\s*(.+)$", line)
+        if match:
             if int(match.group(1)) != 0 or match.group(2) != "xyz":
                 raise Exception(f"Unknown coordinate system: {line}")
-        elif match := re.match(r"^normalisation\[(\d+)\]\s*=\s*(.+)$", line):
+            continue
+        match = re.match(r"^normalisation\[(\d+)\]\s*=\s*(.+)$", line)
+        if match:
             if int(match.group(1)) != 0 or match.group(2) != "unity":
                 raise Exception(f"Unknown normlization: {line}")
-        elif match := re.match(r"^vector\[(\d+)\]\s*=\s*(.+)$", line):
+            continue
+        match = re.match(r"^vector\[(\d+)\]\s*=\s*(.+)$", line)
+        if match:
             index = int(match.group(1))
             value = [
                 float(x) for x in re.split(r"[(,) ]+", match.group(2)) if x]
             sets[current_set][index] = value
-        else:
-            print(f"Unknown line, skipping: {line}")
+            continue
+        
+        print(f"Unknown line, skipping: {line}")
     
     return sets
